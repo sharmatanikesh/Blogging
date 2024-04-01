ca9b1a17f6a9eff1453407003535cc0a94c629e5
sharmatanikesh <sharmatanikesh@gmail.com>
2024-04-01 17:27:45 +0530

    Done

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -12,29 +12,50 @@ export const userRoute = new Hono<{
   }>()
   
   
+  // userRoute.post('/signup', async (c) => {
+  //     const prisma = new PrismaClient({
+  //         datasourceUrl: c.env?.DATABASE_URL	,
+  //     }).$extends(withAccelerate());
+  
+    //  const body = await c.req.json();
+      // try {
+     // console.log(body)
+          // const user = await prisma.user.create({
+          //     data: {
+          //         email: body.email,
+          //         password: body.password,
+          //     }
+          // });
+          // const jwt = await sign({ id: user.id }, c.env.JWT_SECRET); 
+          // return c.json({ jwt });
+      // } catch(e) {
+      //     c.status(403);
+      // console.log(e)
+      //     return c.json({ error: "error while signing up" });
+      // }
+  //})
   userRoute.post('/signup', async (c) => {
-      const prisma = new PrismaClient({
-          datasourceUrl: c.env?.DATABASE_URL	,
-      }).$extends(withAccelerate());
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());  
+    const body = await c.req.json();
+   // const { success } = signupInput.safeParse(body);
+   // if (!success) { c.status(411); return c.json({ message: 'Invalid input' }) }
+   // const hashedPass = await hashFunction(body.password);
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name || '',
+      },
+    });
   
-      const body = await c.req.json();
-      try {
-      console.log(body)
-          const user = await prisma.user.create({
-              data: {
-                  email: body.email,
-                  password: body.password,
-              }
-          });
-          const jwt = await sign({ id: user.id }, c.env.JWT_SECRET); 
-          return c.json({ jwt });
-      } catch(e) {
-          c.status(403);
-      console.log(e)
-          return c.json({ error: "error while signing up" });
-      }
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET)
+  
+    return c.json({
+      jwt: token
+    })
   })
-  
   
   userRoute.post('/signin',async (c)=>{
     const prisma = new PrismaClient({
